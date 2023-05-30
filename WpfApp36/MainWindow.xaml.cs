1dd84bfa2a6c0babca9fd22be85273b55905b9ce
feXXon <RobertGuzaerov123@yandex.ru>
2023-05-30 12:00:11 +0300

    Добавьте файлы проекта.

---
+++ WpfApp36/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp36
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Bustton_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                int a = int.Parse(Boxa.Text);
+                int b = int.Parse(Boxb.Text);
+                int x = 0;
+                for (int i = a; i <= b; i++)
+                {
+                    x += i * i;
+                }
+                txtOtv.Text = $"Сумма квадратов от {a} до {b} = {x}";
+            }
+        }
+    }
+}
